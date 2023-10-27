bb0de592f2f77151d366bfabaf62b279f0bfd31a
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-27 13:14:14 +0300

    created noteActions.js

---
+++ src/store/actions/noteActions.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export const ADD_NOTE = "ADD_NOTE => adds a new note"
+export const REMOVE_NOTE = "REMOVE_NOTE => removes a note"
+
+export function addNote(note) {
+  return { type: ADD_NOTE, payload: note }
+}
+
+export function removeNote(noteId) {
+  return { type: REMOVE_NOTE, payload: noteId}
+}
+
+export const addNoteAPI = (newNote) => dispatch => {
+  axios
+    .post("https://httpbin.org/anything", newNote)
+    .then((res) => {
+      if (res.status === 200) {
+        // res.data objesi içerisinden ihtiyaç duyduğunuz değeri bulun ve oluşturduğunuz notEkle ile dispatch edin
+      }
+    })
+    .catch((error) => console.log(error));
+}
+
+export const removeNoteAPI = (id) => dispatch => {
+  console.log(id)
+  axios
+    .delete("https://httpbin.org/anything", { data: id })
+    .then((res) => {
+      if (res.status === 200) {
+        // res.data objesi içerisinden ihtiyaç duyduğunuz değeri bulun ve oluşturduğunuz notSil ile dispatch edin 
+      }
+    })
+    .catch((error) => console.log(error));
+}
