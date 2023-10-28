a71b1a02646b131aaf3bed533e461452560f4367
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-28 15:33:22 +0300

    css edited

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ import Img from "./assets/gratitude.jpg";
 export default function App() {
   return (
     <div>
-      <div className="bg-white shadow mb-8">
-        <nav className="flex gap-2 justify-center max-w-xl mx-auto text-zinc-500">
+      <div className="bg-white shadow mb-8 ">
+        <nav className="flex gap-2 justify-center max-w-xl mx-auto text-zinc-500 ">
           <NavLink
             to="/"
             exact
-            className="p-4 pb-3 tracking-tighter"
+            className="p-4 pb-3 tracking-tighter hover:bg-[#ffe4a9] rounded"
             activeClassName="text-amber-600"
           >
             Anasayfa
           </NavLink>
           <NavLink
             to="/notlar"
-            className="p-4 pb-3 tracking-tighter"
+            className="p-4 pb-3 tracking-tighter hover:bg-[#ffe4a9] rounded"
             activeClassName="text-amber-600"
           >
             Tüm Notlar
           </NavLink>
           <NavLink
             to="/yeni-not"
-            className="p-4 pb-3 tracking-tighter"
+            className="p-4 pb-3 tracking-tighter hover:bg-[#ffe4a9] rounded"
             activeClassName="text-amber-600"
           >
             Yeni Not
@@ -35,9 +35,9 @@ export default function App() {
       </div>
       <Switch>
         <Route exact path="/">
-          <div className="max-w-xl mx-auto px-4 pb-8">
-            <div className="beyazKutu">
-              <img src={Img} alt="" />
+          <div className="max-w-xl mx-auto px-4 pb-8 rounded-lg">
+            <div className="beyazKutu rounded-md">
+              <img src={Img} alt="" className="rounded-t-lg"/>
               <div className="px-8 pt-7 pb-6 text-sm">
                 <h1 className="text-3xl text-amber-600">
                   Minnet Günlüğü Nedir?
